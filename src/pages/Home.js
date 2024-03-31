0da5a4c4916e204eb6c626394caa978b091c2a32
Nahason <nahason.murithi@student.moringaschool.com>
2024-03-31 18:18:57 +0300

    partial home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import { Box } from '@mui/material';
 
+import HomeBanner from '../components/HomeBanner';
+import SearchExercises from '../components/SearchExercises';
+import Exercises from '../components/Exercises';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HomeBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
   )
 }
 
